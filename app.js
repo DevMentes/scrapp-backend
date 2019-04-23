fedd2af84ae766ccd23bb85331c2b954153e16dd
camilo alaniz <calaniz@option.cl>
2019-04-23 09:45:23 -0400

    added cors support

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const parser = require('body-parser');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const modules = require('./src/contexts/index');
+const cors = require('cors');
 
 const app = express();
 
 app.use(helmet());
 app.use(morgan('dev'));
 app.use(parser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json({
